c3c65d8a9bf5709fd51e338ccc514e25e5239f82
ElenaChka <checkalina.lena@bk.ru>
2021-03-14 10:39:27 +0300

    исправленная

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
+//программа, которая выводит числа от 1 до 10
 #include<iostream>
 using namespace std;
 int main()
 {
     for( int i=1;i<=10;i++)
     {
-        cout<<i<<endl ;
+        cout<<i<<endl;
     }
 }
